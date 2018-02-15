5d2c7427d5141ae2a101b80985cd4d74d1e1eb87
Kevin Ushey <kevinushey@gmail.com>
2018-02-15 12:32:03 -0800

    add reticulate hook

--- src/cpp/session/SessionMain.cpp
+++ src/cpp/session/SessionMain.cpp
@@ -166,6 +166,7 @@
 #include "modules/mathjax/SessionMathJax.hpp"
 #include "modules/SessionLibPathsIndexer.hpp"
 #include "modules/SessionObjectExplorer.hpp"
+#include "modules/SessionReticulate.hpp"
 
 #include <session/SessionProjectTemplate.hpp>
 
@@ -506,6 +507,7 @@ Error rInit(const rstudio::r::session::RInitInfo& rInitInfo)
       (modules::rstudioapi::initialize)
       (modules::libpaths::initialize)
       (modules::explorer::initialize)
+      (modules::reticulate::initialize)
 
       // workers
       (workers::web_request::initialize)
